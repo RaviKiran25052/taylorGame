fe43e385db61b5424c704900da8d1a541bd20a0e
Ravi Kiran <113467994+RaviKiran25052@users.noreply.github.com>
2024-12-02 16:37:09 +0530

    lock

--- script.js
+++ script.js
@@ -75,12 +75,12 @@ wordContainer.addEventListener('click', (e) => {
         dummy.classList.add('vanish');
         setTimeout(() => dummy.remove(), 500);
       });
-      
+
       setTimeout(() => {
         hiddenMessage.classList.add('revealed');
         popupCont.classList.add("show")
       }, 1000);
-      
+
       yesButton.addEventListener('click', () => {
         popupCont.classList.add('hide');
         audio.play();
@@ -110,21 +110,21 @@ let currentY = null;
 noButton.addEventListener('mouseenter', () => {
   let randomIndex;
   let newPosition;
-  
+
   // Keep picking a new position until it's different from the current one
   do {
     randomIndex = Math.floor(Math.random() * positions.length);
     newPosition = positions[randomIndex];
   } while (newPosition.x === currentX && newPosition.y === currentY);
-  
+
   // Update the CSS variables
   currentX = newPosition.x;
   currentY = newPosition.y;
-  
+
   noButton.style.setProperty('--x', `${currentX}px`);
   noButton.style.setProperty('--y', `${currentY}px`);
   noButton.classList.add('smooth-move');
-  
+
   clearTimeout(timeoutId);
 });
 
@@ -143,80 +143,80 @@ var captureContainers;
 var captureIndex;
 
 function init() {
-	// create a new stage and point it at our canvas:
-	canvas = document.getElementById("testCanvas");
-	stage = new createjs.Stage(canvas);
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+  // create a new stage and point it at our canvas:
+  canvas = document.getElementById("testCanvas");
+  stage = new createjs.Stage(canvas);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-	var w = canvas.width;
-	var h = canvas.height;
+  var w = canvas.width;
+  var h = canvas.height;
 
-	container = new createjs.Container();
-	stage.addChild(container);
+  container = new createjs.Container();
+  stage.addChild(container);
 
-	captureContainers = [];
-	captureIndex = 0;
+  captureContainers = [];
+  captureIndex = 0;
 
-	// create a large number of slightly complex vector shapes, and give them random positions and velocities:
-	for (var i = 0; i < 150; i++) {
-		var heart = new createjs.Shape();
-		heart.graphics.beginFill(createjs.Graphics.getHSL(330 + Math.random() * 30, 50 + Math.random() * 30, 75 + Math.random() * 10));
+  // create a large number of slightly complex vector shapes, and give them random positions and velocities:
+  for (var i = 0; i < 150; i++) {
+    var heart = new createjs.Shape();
+    heart.graphics.beginFill(createjs.Graphics.getHSL(330 + Math.random() * 30, 50 + Math.random() * 30, 75 + Math.random() * 10));
     // heart.graphics.beginFill(createjs.Graphics.getHSL(330 + Math.random() * 30, 50 + Math.random() * 30, 75 + Math.random() * 10));
     // heart.graphics.beginFill(createjs.Graphics.getHSL(Math.random() * 30 - 45, 100, 50 + Math.random() * 30));
-		heart.graphics.moveTo(0, -12).curveTo(1, -20, 8, -20).curveTo(16, -20, 16, -10).curveTo(16, 0, 0, 12);
-		heart.graphics.curveTo(-16, 0, -16, -10).curveTo(-16, -20, -8, -20).curveTo(-1, -20, 0, -12);
-		heart.y = -100;
+    heart.graphics.moveTo(0, -12).curveTo(1, -20, 8, -20).curveTo(16, -20, 16, -10).curveTo(16, 0, 0, 12);
+    heart.graphics.curveTo(-16, 0, -16, -10).curveTo(-16, -20, -8, -20).curveTo(-1, -20, 0, -12);
+    heart.y = -100;
 
-		container.addChild(heart);
-	}
+    container.addChild(heart);
+  }
 
-	for (i = 0; i < 100; i++) {
-		var captureContainer = new createjs.Container();
-		captureContainer.cache(0, 0, w, h);
-		captureContainers.push(captureContainer);
-	}
+  for (i = 0; i < 100; i++) {
+    var captureContainer = new createjs.Container();
+    captureContainer.cache(0, 0, w, h);
+    captureContainers.push(captureContainer);
+  }
 
-	// start the tick and point it at the window so we can do some work before updating the stage:
-	createjs.Ticker.timingMode = createjs.Ticker.RAF;
-	createjs.Ticker.on("tick", tick);
+  // start the tick and point it at the window so we can do some work before updating the stage:
+  createjs.Ticker.timingMode = createjs.Ticker.RAF;
+  createjs.Ticker.on("tick", tick);
 }
 
 function tick(event) {
-	var w = canvas.width;
-	var h = canvas.height;
-	var l = container.numChildren;
+  var w = canvas.width;
+  var h = canvas.height;
+  var l = container.numChildren;
 
-	captureIndex = (captureIndex + 1) % captureContainers.length;
-	stage.removeChildAt(0);
-	var captureContainer = captureContainers[captureIndex];
-	stage.addChildAt(captureContainer, 0);
-	captureContainer.addChild(container);
+  captureIndex = (captureIndex + 1) % captureContainers.length;
+  stage.removeChildAt(0);
+  var captureContainer = captureContainers[captureIndex];
+  stage.addChildAt(captureContainer, 0);
+  captureContainer.addChild(container);
 
-	for (var i = 0; i < l; i++) {
-		 var heart = container.getChildAt(i);
+  for (var i = 0; i < l; i++) {
+    var heart = container.getChildAt(i);
 
-		 if (heart.y < -50) {
-			  heart._x = Math.random() * w;
-			  heart.y = h * (1 + Math.random()) + 50;
-			  heart.perX = (1 + Math.random() * 2) * h;
-			  heart.offX = Math.random() * h;
-			  heart.ampX = heart.perX * 0.1 * (0.15 + Math.random());
-			  heart.velY = -Math.random() * 4 - 2; // INCREASED SPEED
-			  heart.scale = Math.random() * 2 + 1;
-			  heart._rotation = Math.random() * 40 - 20;
-			  heart.alpha = Math.random() * 0.75 + 0.05;
-			  heart.compositeOperation = Math.random() < 0.33 ? "lighter" : "source-over";
-		 }
+    if (heart.y < -50) {
+      heart._x = Math.random() * w;
+      heart.y = h * (1 + Math.random()) + 50;
+      heart.perX = (1 + Math.random() * 2) * h;
+      heart.offX = Math.random() * h;
+      heart.ampX = heart.perX * 0.1 * (0.15 + Math.random());
+      heart.velY = -Math.random() * 4 - 2; // INCREASED SPEED
+      heart.scale = Math.random() * 2 + 1;
+      heart._rotation = Math.random() * 40 - 20;
+      heart.alpha = Math.random() * 0.75 + 0.05;
+      heart.compositeOperation = Math.random() < 0.33 ? "lighter" : "source-over";
+    }
 
-		 var int = (heart.offX + heart.y) / heart.perX * Math.PI * 2;
-		 heart.y += heart.velY * heart.scaleX / 2;
-		 heart.x = heart._x + Math.cos(int) * heart.ampX;
-		 heart.rotation = heart._rotation + Math.sin(int) * 30;
-	}
+    var int = (heart.offX + heart.y) / heart.perX * Math.PI * 2;
+    heart.y += heart.velY * heart.scaleX / 2;
+    heart.x = heart._x + Math.cos(int) * heart.ampX;
+    heart.rotation = heart._rotation + Math.sin(int) * 30;
+  }
 
-	captureContainer.updateCache("source-over");
-	stage.update(event);
+  captureContainer.updateCache("source-over");
+  stage.update(event);
 }
 
 function calculateMonthsAndDays(startDate, endDate) {
@@ -241,3 +241,111 @@ const result = calculateMonthsAndDays(startDate, today);
 
 // Set the value in the HTML element
 document.getElementById("duration").textContent = `${result.months} months ${result.days} days,`;
+
+const containerCard = document.querySelector('.container-card');
+const buttonToggle = document.getElementById('toggleButton');
+const cardGlass = document.getElementById('card');
+const newMessageDiv = document.getElementById('main-container');
+let isOpen = false;
+
+buttonToggle.addEventListener('click', () => {
+  if (!isOpen) {
+    cardGlass.style.height = '350px';
+    cardGlass.querySelector('.card-text-content').style.opacity = '1';
+  } else {
+    cardGlass.style.height = '0';
+    cardGlass.querySelector('.card-text-content').style.opacity = '0';
+  }
+  isOpen = !isOpen;
+});
+
+const contactNameInput = document.getElementById('contact-name');
+
+const daySelect = document.getElementById("day");
+const monthSelect = document.getElementById("month");
+const yearSelect = document.getElementById("year");
+
+const submitButton = document.getElementById('submit-btn');
+
+for (let i = 1; i <= 31; i++) {
+  const option = document.createElement("option");
+  option.value = i;
+  option.textContent = i;
+  daySelect.appendChild(option);
+}
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+months.forEach((month, index) => {
+  const option = document.createElement("option");
+  option.value = index + 1;
+  option.textContent = month;
+  monthSelect.appendChild(option);
+});
+
+const currentYear = new Date().getFullYear();
+for (let i = currentYear; i >= currentYear - 1; i--) {
+  const option = document.createElement("option");
+  option.value = i;
+  option.textContent = i;
+  yearSelect.appendChild(option);
+}
+
+function isInputFilled() {
+  return contactNameInput.value.trim() !== '' &&
+    daySelect.value !== '' &&
+    monthSelect.value !== '' &&
+    yearSelect.value !== '';
+}
+
+submitButton.addEventListener('mouseenter', () => {
+  if (!isInputFilled()) {
+    if (submitButton.classList.contains('right-button')) {
+      submitButton.classList.remove('right-button');
+      submitButton.classList.add('left-button');
+    } else {
+      submitButton.classList.remove('left-button');
+      submitButton.classList.add('right-button');
+    }
+  }
+});
+
+function checkInputAndDate() {
+  const name = contactNameInput.value.trim().toLowerCase(); // Convert name input to lowercase
+  const selectedDay = parseInt(daySelect.value);
+  const selectedMonth = parseInt(monthSelect.value);
+  const selectedYear = parseInt(yearSelect.value);
+
+  if ((name === 'taylor swift' || name === 'taylorswift') && selectedDay === 30 || 17 && selectedMonth === 5 || 7 && selectedYear === 2024) {
+    buttonToggle.textContent = '🔓';
+    buttonToggle.style.border = 'solid 3px green';
+    buttonToggle.style.boxShadow = '0 0 15px rgb(0, 255, 0)';
+    setTimeout(() => {
+      containerCard.style.opacity = 0;
+    }, 500);
+
+    // After fade out is complete, show the new message
+    setTimeout(() => {
+      containerCard.classList.add('fade-out-effect');
+      newMessageDiv.classList.remove('hidden-element');
+      newMessageDiv.classList.add('fade-in-effect');
+    }, 1500); // Wait for fade out to complete
+  } else {
+    buttonToggle.classList.add('lock'); // Add the lock class
+    setTimeout(() => {
+      buttonToggle.classList.remove('lock'); // Remove the class after animation ends
+    }, 500);
+    setTimeout(() => {
+      cardGlass.querySelector('.card-text-content').style.opacity = '0';
+      cardGlass.style.height = '0';
+      contactNameInput.value = '';
+      daySelect.value = '';
+      monthSelect.value = '';
+      yearSelect.value = '';
+      isOpen = !isOpen;
+    }, 1000);
+  }
+}
+
+submitButton.addEventListener('click', () => {
+  checkInputAndDate();
+});
